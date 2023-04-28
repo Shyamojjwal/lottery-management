cce11df28c727397d364fef195c8e3b24ca594e3
shyamojjwal-matrix <shyamojjwal.s@appnosticworx.com>
2023-04-28 17:30:12 +0530

    User Modification

--- src/app/pages/featured-pages/user-management/components/user-modification/user-modification.component.ts
+++ src/app/pages/featured-pages/user-management/components/user-modification/user-modification.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-modification',
@@ -12,21 +13,37 @@ export class UserModificationComponent implements OnInit {
   public isPreview: boolean = false;
   public isModify: boolean = false;
 
+  public userModifyForm: FormGroup | any;
+
+  private userCode: any = null;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private FB: FormBuilder,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.isNewEntry = this.router.url.includes('new-user');
-    console.log("this.isNewEntry: ", this.isNewEntry)
+    this.userCode = this.activatedRoute.snapshot.params['userCode'] || null;
+
+    this.initModifyForm();
+  
     if(this.isNewEntry) {
       this.isPreview = false;
       this.isModify = true;
     } else {
       this.isPreview = true;
       this.isModify = false;
+      this.loadUserInfo();
     }
   }
+
+  initModifyForm = () => {
+    this.userModifyForm = this.FB.group({})
+  }
+
+  loadUserInfo = () => {}
 
   continueInfoModification = () => {
     this.isPreview = false;
